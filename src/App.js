e71e1001f840bfe08f1b2db9bce50e52e41190b6
Jainil <shahjainilb@gmail.com>
2023-02-09 17:45:31 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Profile from './components/Profile';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />}></Route>
