09bf9e17bfe4984bd68f02da19a1cab34d2c5a35
Jainil <shahjainilb@gmail.com>
2023-02-10 02:46:02 +0530

    login done

--- Backend/models/Company.js
+++ Backend/models/Company.js
@@ -1,6 +1,4 @@
-
 const mongoose = require("mongoose");
-// import autoIncrement from "mongoose-auto-increment"
 
 const CompanyDataSchema = new mongoose.Schema({
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
-// import logo from "../../assets/Logo.jpg";
-// import google from "../../assets/google.svg";
-// import github from "../../assets/github.svg";
-// import signup from "../../assets/signup.svg";
-// import axios from "../../axios";
-import { useNavigate } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import axios from "../../axios";
+import { useNavigate, Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
   const navigate = useNavigate() ;
   const [input, setInput] = useState({
-    role: "",
     email: "",
     password: "",
   });
@@ -26,26 +23,17 @@ const Login = () => {
 
   const SubmitHandler = async (e) => {
     e.preventDefault();
-    const user = {email: input.email, password : input.password ,role:input.role}
-    const role = input.role;
-    // try {
-    //   if(role=="student"){
-    //     const res = await axios.post("/student/login", user);
-    //     console.log(res.data);
-    //     localStorage.setItem("signature",res.data.signature)
-    //     navigate("/")
-    //   }
-    //   else {
-    //     const res = await axios.post("/teacher/login", user);
-    //     console.log(res.data);
-    //     localStorage.setItem("signature",res.data.signature)
-    //     navigate("/dashboard2")
-    //   }
-    // }catch(error) {
-    //   console.log("error form content", error)
-    // }
+    const user = { Email: input.email, Password : input.password }
+    try {
+      if(user){
+        const res = await axios.post("/login/login", user);
+        console.log(res.data);
+        navigate("/")
+      }
+    }catch (error){
+      console.log("error form content", error)
+    }
     setInput({
-      role: "",
       email: "",
       password: "",
     });
@@ -53,103 +41,28 @@ const Login = () => {
 
   return (
     <>
-      {/* <form onSubmit={SubmitHandler}>
-        <div className="max-h-screen bg-hero text-gray-900 flex justify-center">
-          <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-            <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-4">
-              <div>
-                <img src={logo} className="w-32 mx-auto" alt="" />
-              </div>
-              <div className="mt-4 flex flex-col items-center">
-                <h1 className="text-2xl font-extrabold text-poppins">
-                  Sign In with V-Learn
-                </h1>
-                <div class="w-full flex-1 mt-5">
-                  <div class="flex flex-col items-center">
-                    <button class="w-full max-w-xs font-bold shadow-sm rounded-lg py-2 bg-[#D4D9F6] text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
-                      <div class="bg-white p-2 rounded-full">
-                        <img class="w-4" src={google} alt="" />
-                      </div>
-                      <span class="ml-4">Sign In with Google</span>
-                    </button>
-
-                    <button class="w-full max-w-xs font-bold shadow-sm rounded-lg py-2 bg-[#D4D9F6] text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
-                      <div class="bg-white p-1 rounded-full">
-                        <img class="w-6" src={github} alt="" />
-                      </div>
-                      <span class="ml-4">Sign In with GitHub</span>
-                    </button>
-                  </div>
-
-                  <div class="my-2 border-b text-center">
-                    <div class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                      Or sign up with e-mail
-                    </div>
-                  </div>
-
-                  <div class="mx-auto mt-2 max-w-xs">
-                    <input
-                      name="role"
-                      class="w-full px-8 py-2 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                      type="text"
-                      placeholder="role"
-                      value={input.role}
-                      onChange={InputHandler}
-                      required
-                    />
-                    <input
-                      name="email"
-                      class="w-full px-8 py-2 mt-5 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                      type="email"
-                      placeholder="Email"
-                      value={input.email}
-                      onChange={InputHandler}
-                      reaquired
-                    />
-                    <input
-                      name="password"
-                      class="w-full px-8 py-2 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                      type="password"
-                      placeholder="Set Password"
-                      value={input.password}
-                      onChange={InputHandler}
-                      required
-                    />
-                    <button class="mt-3 tracking-wide font-semibold bg-[#233FF2] text-gray-100 w-full py-3 rounded-lg hover:bg-[#1D33AA] transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                      <img className="fill-white w-4 h-4" src={signup} alt="" />
-                      <span class="ml-3">Sign In</span>
-                    </button>
-                    <div className="flex mt-2">
-                      <div class=" text-xs text-gray-600">
-                        Not Signed In? Sign Up as{" "}
-                        <a
-                          href="/signupteacher"
-                          class="border-b border-gray-500 border-dotted"
-                        >
-                          Teacher
-                        </a>{" "}
-                        or{" "}
-                        <a
-                          href="/signupstudent"
-                          class="border-b border-gray-500 border-dotted"
-                        >
-                          Student
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="flex-1 bg-[#D4D9F6] text-center hidden lg:flex">
-              <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat align-middle items-center">
-                <img src={bg} alt="" />
-              </div>
-            </div>
-          </div>
+    <div className="wrapper">
+         <div className="logo">
+            <img src={logo} alt=""/>
+        </div> 
+        <div className="text-center mt-4 name">
+            Sponsor Spot
         </div>
-      </form> */}
-      login page
+        <form className="p-3 mt-3">
+            <div className="form-field d-flex align-items-center">
+                <span className="far fa-user"></span>
+                <input type="text" name="email" value={input.email} onChange={InputHandler} id="userName" placeholder="Email"/>
+            </div>
+            <div className="form-field d-flex align-items-center">
+                <span className="fas fa-key"></span>
+                <input type="password" name="password" value={input.password} onChange={InputHandler} id="pwd" placeholder="Password"/>
+            </div>
+            <button onClick={SubmitHandler} className="btn mt-3">Login</button>
+        </form>
+        <div className="text-center fs-6">
+            <Link to="/">Forget password?</Link> <br /> <Link to="/signup">Sign up</Link>
+        </div>
+    </div>
     </>
   );
 };
