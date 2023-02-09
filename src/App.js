5b015c38c8df98237fffd74ea9aa5476ace431ce
Jainil <shahjainilb@gmail.com>
2023-02-09 15:08:30 +0530

    basic client setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import Profile from './components/Profile';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/profile" element={<Profile />}> </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
